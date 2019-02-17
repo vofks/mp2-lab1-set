ab72f3fcd182c33bde1bf9d108bdc865be03d5d4
Vofks <vofksden@gmail.com>
2019-02-17 15:33:19 +0300

    tset.cpp finished (not tested)

--- src/tset.cpp
+++ src/tset.cpp
@@ -1,99 +1,132 @@
-// ННГУ, ВМК, Курс "Методы программирования-2", С++, ООП
-//
-// tset.cpp - Copyright (c) Гергель В.П. 04.10.2001
-//   Переработано для Microsoft Visual Studio 2008 Сысоевым А.В. (19.04.2015)
-//
-// Множество - реализация через битовые поля
-
 #include "tset.h"
 
-TSet::TSet(int mp) : BitField(-1)
+TSet::TSet(int mp) : bitField(mp)
 {
+  maxPower = mp;
 }
 
-// конструктор копирования
-TSet::TSet(const TSet &s) : BitField(-1)
+TSet::TSet(const TSet &s) : bitField(s.bitField)
 {
+  maxPower = s.maxPower;
 }
 
-// конструктор преобразования типа
-TSet::TSet(const TBitField &bf) : BitField(-1)
+TSet::TSet(const TBitField &bf) : bitField(bf)
 {
+  maxPower = bitField.GetLength();
 }
 
 TSet::operator TBitField()
 {
-	return TBitField(0);
+  return bitField;
 }
 
-int TSet::GetMaxPower(void) const // получить макс. к-во эл-тов
+int TSet::GetMaxPower(void) const
 {
-	return 0;
+  return maxPower;
 }
 
-int TSet::IsMember(const int Elem) const // элемент множества?
+int TSet::IsMember(const int Elem) const
 {
+  if (Elem < 0 || Elem >= maxPower)
+    throw "Incorrect element";
+  return bitField.GetBit(Elem);
+}
+
+void TSet::InsElem(const int Elem)
+{
+  if (Elem < 0 || Elem >= maxPower)
+    throw "Incorrect element";
+  bitField.SetBit(Elem);
+}
+
+void TSet::DelElem(const int Elem)
+{
+  if (Elem < 0 || Elem >= maxPower)
+    throw "Incorrect element";
+  bitField.ClrBit(Elem);
+}
+
+TSet& TSet::operator=(const TSet &s)
+{
+  maxPower = s.maxPower;
+  bitField = s.bitField;
+  return *this;
+}
+
+int TSet::operator==(const TSet &s) const
+{
+  if (maxPower != s.maxPower)
     return 0;
+  else
+    if (bitField != s.bitField)
+      return 0;
+  return 1;
 }
 
-void TSet::InsElem(const int Elem) // включение элемента множества
+int TSet::operator!=(const TSet &s) const
 {
+  if (maxPower != s.maxPower)
+    return 1;
+  else
+    if (bitField != s.bitField)
+      return 1;
+  return 0;
 }
 
-void TSet::DelElem(const int Elem) // исключение элемента множества
+TSet TSet::operator+(const TSet &s)
 {
+  int len = maxPower;
+  if (s.maxPower > maxPower)
+    len = s.maxPower;
+  TSet temp(len);
+  temp.bitField = bitField | s.bitField;
+  return temp;
 }
 
-// теоретико-множественные операции
-
-TSet& TSet::operator=(const TSet &s) // присваивание
+TSet TSet::operator+(const int Elem)
 {
-	return TSet(0);
+  if (Elem < 0 || Elem >= maxPower)
+    throw "Incorrect element";
+  bitField.SetBit(Elem);
+  return *this;
 }
 
-int TSet::operator==(const TSet &s) const // сравнение
+TSet TSet::operator-(const int Elem)
 {
-    return 0;
+  if (Elem < 0 || Elem >= maxPower)
+    throw "Incorrect element";
+  bitField.ClrBit(Elem);
+  return *this;
 }
 
-int TSet::operator!=(const TSet &s) const // сравнение
+TSet TSet::operator*(const TSet &s)
 {
-	return 0;
+  int len = maxPower;
+  if (s.maxPower > maxPower)
+    len = s.maxPower;
+  TSet temp(len);
+  temp.bitField = bitField & s.bitField;
+  return temp;
 }
 
-TSet TSet::operator+(const TSet &s) // объединение
+TSet TSet::operator~(void)
 {
-	return TSet(0);
+  TSet temp = *this;
+  temp.bitField = ~temp.bitField;
+  return temp;
 }
 
-TSet TSet::operator+(const int Elem) // объединение с элементом
+istream &operator>>(istream &istr, TSet &s)
 {
-	return TSet(0);
+  int i;
+  cin >> i;
+  for (; ((i > -1) && (i < s.maxPower)); cin >> i)
+    s.bitField.SetBit(i);
+  return istr;
 }
 
-TSet TSet::operator-(const int Elem) // разность с элементом
+ostream& operator<<(ostream &ostr, const TSet &s)
 {
-	return TSet(0);
-}
-
-TSet TSet::operator*(const TSet &s) // пересечение
-{
-	return TSet(0);
-}
-
-TSet TSet::operator~(void) // дополнение
-{
-	return TSet(0);
-}
-
-// перегрузка ввода/вывода
-
-istream &operator>>(istream &istr, TSet &s) // ввод
-{
-	return istream(0);
-}
-
-ostream& operator<<(ostream &ostr, const TSet &s) // вывод
-{
-	return ostream(0);
+  ostr << s.bitField << ' ';
+  return ostr;
 }
